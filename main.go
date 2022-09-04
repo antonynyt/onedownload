2f3a8dbe7ebe6beadd11a90aa3f2587049c4d57e
Antony <antony.neyret@gmail.com>
2022-09-04 19:35:28 +0200

    feat: handle basic download to file

--- main.go
+++ main.go
@@ -1,37 +1,53 @@
 package main
 
 import (
-		"flag"
-		"fmt"
-		"encoding/base64"
-		"strings"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
 )
 
-func main(){
+func main() {
 
-sharedUrl := flag.String("url", "", "The OneDrive Shared URL.")
-flag.Parse()
+	sharedUrl := flag.String("url", "", "The OneDrive Shared URL.")
+	outfile := flag.String("o", "", "Name of the output file.")
+	flag.Parse()
 
-base64 := base64.StdEncoding.EncodeToString([]byte(*sharedUrl))
-encodedUrl := "u!" + strings.Trim(base64, "=")
-encodedUrl = strings.Replace(encodedUrl, "/", "_", -1)
-encodedUrl = strings.Replace(encodedUrl, "+", "-", -1)
+	if *sharedUrl == "" || *outfile == "" {
+		fmt.Println("mandatory arguments -url -o")
+		os.Exit(1)
+	}
 
-url := "https://api.onedrive.com/v1.0/shares/" + encodedUrl + "/root/content"
+	base64 := base64.StdEncoding.EncodeToString([]byte(*sharedUrl))
+	encodedUrl := "u!" + strings.Trim(base64, "=")
+	encodedUrl = strings.Replace(encodedUrl, "/", "_", -1)
+	encodedUrl = strings.Replace(encodedUrl, "+", "-", -1)
 
-fmt.Println(url)
+	url := "https://api.onedrive.com/v1.0/shares/" + encodedUrl + "/root/content"
+
+	fmt.Printf("Download url: %s", url)
+
+	out, err := os.Create(*outfile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer out.Close()
+
+	resp, _ := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 }
-
-/*
-
-$sharedUrl = urldecode($_GET['url']);
-$base64 = base64_encode($sharedUrl);
-$encodedUrl = "u!" . rtrim($base64, '=');
-$encodedUrl = str_replace('/', '_', $encodedUrl);
-$encodedUrl = str_replace('+', '-', $encodedUrl);
-
-$final = sprintf('https://api.onedrive.com/v1.0/shares/%s/root/content', $encodedUrl);
-header('Location:' . $final, true, 302);
-
-*/

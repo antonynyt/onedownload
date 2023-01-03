574a9c53e94c8b085dd0eeaf3ce98f6c1393be33
Antony <antony.neyret@gmail.com>
2023-01-03 23:16:05 +0100

    Feat: add a progress bar

--- main.go
+++ main.go
@@ -9,16 +9,14 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
 )
 
 /*
 Todo
-- Clear the code, better error handling
-- Better http download
-- Show the current status of the download like in curl + estimated time (content length)
-- Get the http body and the filename from the head
+- clear the sharedUrl of any ?parameters
+- Don't download if file already exists
 - Don't print url if status != 200ok
-- Separate code in functions
 - Download Folder?
 */
 
@@ -58,7 +56,7 @@ func main() {
 	url := formatURL(sharedUrl)
 
 	//Print the full link
-	fmt.Printf("[-] Full link: %s\n", url)
+	//fmt.Printf("[-] Full link: %s\n", url)
 
 	//
 	item, err := getItem(url)
@@ -71,13 +69,11 @@ func main() {
 		*outfile = item.Name
 	}
 
-	/*
-		fmt.Println("Name:", item.Name)
-		fmt.Println("Size:", item.Size)
-		fmt.Println("URL:", item.URL)
-	*/
-
-	downloadItem(item.URL, *outfile)
+	err = downloadItem(item.URL, *outfile)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
 
 func formatURL(sharedUrl *string) string {
@@ -139,6 +135,9 @@ func downloadItem(url, filename string) error {
 		return fmt.Errorf("failed to download file: %s", resp.Status)
 	}
 
+	//total size of the file
+	totalBytes := resp.ContentLength
+
 	//create the outfile
 	out, err := os.Create(filename)
 	if err != nil {
@@ -146,11 +145,59 @@ func downloadItem(url, filename string) error {
 	}
 	defer out.Close()
 
+	go downloadProgress(filename, totalBytes)
+
 	//Write the contents of the response to the output file.
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func downloadProgress(path string, totalSize int64) error {
+
+	// Size of the progress bar
+	const barWidth = 40
+
+	// Set up a timer to update the progress every 100ms.
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+
+	// Open the file.
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	for {
+
+		// Get the size of the file.
+		fi, err := file.Stat()
+		if err != nil {
+			return err
+		}
+		size := fi.Size()
+
+		// Update the progress. downloading [=======--------] 50%
+		select {
+		case <-ticker.C:
+			percent := float64(size) / float64(totalSize) * 100
+			barSize := int(percent) * barWidth / 100
+			bar := strings.Repeat("=", barSize) + strings.Repeat("-", 40-barSize)
+			fmt.Printf("Downloading [%s] %.0f%%\r", bar, percent)
+		default:
+		}
+
+		// Break out of the loop when the file is fully downloaded.
+		if size == totalSize {
+			break
+		}
+	}
+
+	fmt.Println()
 
 	return nil
 }

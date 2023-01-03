1d4397dda5b89fe35b43d9795f0fee0d0f8e75ad
Antony <antony.neyret@gmail.com>
2023-01-03 19:28:44 +0100

    Separated code into func

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/fatih/color"
 )
 
 /*
@@ -23,92 +22,135 @@ Todo
 - Download Folder?
 */
 
+const (
+	apiURL = "https://api.onedrive.com/v1.0/"
+)
+
+type Item struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Size int64  `json:"size"`
+	URL  string `json:"@content.downloadUrl"`
+}
+
 func main() {
 	//set the flags
 	onlyURL := flag.Bool("nodownload", false, "Optional: generate only the download link.")
-	outfile := flag.String("o", "", "Set the output `filename`. E.g. video.mkv.")
+	outfile := flag.String("o", "", "Set the output `filename`. Example: (image.png)")
 	sharedUrl := flag.String("url", "", "The OneDrive Share `link`.")
 	flag.Parse()
 
 	//if no flags set throw an error
 	if !*onlyURL {
-		if *sharedUrl == "" || *outfile == "" {
+		if *sharedUrl == "" {
 			flag.Usage()
-			os.Exit(0)
+			os.Exit(1)
 		}
 	} else if *onlyURL && *sharedUrl != "" {
-		printBanner()
-		fmt.Printf("[-] Full link: %s\n", linkFormatter(sharedUrl))
+		fmt.Printf("[-] Full link: %s\n", formatURL(sharedUrl))
 		return
 	} else {
 		flag.Usage()
-		os.Exit(0)
+		os.Exit(1)
 	}
 
-	printBanner()
-
 	//format the shared OneDrive Url
-	url := linkFormatter(sharedUrl)
-	//url := *sharedUrl
+	url := formatURL(sharedUrl)
 
 	//Print the full link
 	fmt.Printf("[-] Full link: %s\n", url)
 
-	//HTTP GET
-	//GET THE CONTENT
-	resp, err := http.Get(url)
+	//
+	item, err := getItem(url)
 	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	//check if 200 OK
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Could not get the file, status: %s\n", resp.Status)
-		return
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
 
-	//create the outfile
-	out, err := os.Create(*outfile)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer out.Close()
-
-	//COPY THE CONTENT OF THE GET INTO THE FILE
-
-	size, err := io.Copy(out, resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if *outfile == "" {
+		*outfile = item.Name
 	}
 
-	fmt.Println(size)
+	/*
+		fmt.Println("Name:", item.Name)
+		fmt.Println("Size:", item.Size)
+		fmt.Println("URL:", item.URL)
+	*/
 
-	fmt.Println()
+	downloadItem(item.URL, *outfile)
 }
 
-func linkFormatter(sharedUrl *string) string {
+func formatURL(sharedUrl *string) string {
 	//genrate the download url from OneDrive API
 	base64 := base64.StdEncoding.EncodeToString([]byte(*sharedUrl))
 	encodedUrl := "u!" + strings.Trim(base64, "=")
 	encodedUrl = strings.Replace(encodedUrl, "/", "_", -1)
 	encodedUrl = strings.Replace(encodedUrl, "+", "-", -1)
 
-	url := "https://api.onedrive.com/v1.0/shares/" + encodedUrl + "/root/content"
+	//only for files /DriveItem
+	//?$expand=children show the childrens
+	url := apiURL + "shares/" + encodedUrl + "/driveItem"
 
 	return url
 }
 
-func printBanner() {
-	color.Cyan("--------------------")
-	color.Cyan("OneDownloader v.0.1")
-	color.Cyan("--------------------")
-	println()
+func getItem(url string) (*Item, error) {
+	//HTTP GET
+	// Set up the request.
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Send the request.
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Check the status code.
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to get item: %s", resp.Status)
+	}
+
+	// Decode the response body.
+	var item Item
+	err = json.NewDecoder(resp.Body).Decode(&item)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode json: %s", resp.Status)
+	}
+
+	return &item, nil
 }
 
-/*
+func downloadItem(url, filename string) error {
 
-&{200 OK 200 HTTP/2.0 2 0 map[Accept-Ranges:[bytes] Cache-Control:[public] Content-Disposition:[attachment; filename="1-221.png"] Content-Length:[9635415] Content-Location:[REDACTED] Content-Type:[image/png] Ctag:[] Date:[Tue, 06 Sep 2022 20:43:54 GMT] Etag:[] Expires:[Mon, 05 Dec 2022 20:43:53 GMT] Last-Modified:[Thu, 10 Jun 2021 19:14:03 GMT] Ms-Cv:[.0] P3p:[CP="BUS CUR CONo FIN IVDo ONL OUR PHY SAMo TELo"] Strict-Transport-Security:[max-age=31536000; includeSubDomains] X-Asmversion:[UNKNOWN; 19...2005] X-Cache:[CONFIG_NOCACHE] X-Content-Type-Options:[nosniff] X-Msedge-Ref:[Ref A:  Ref B:  Ref C: 2022-09-06T20:43:51Z] X-Msnserver:[] X-Preauthinfo:[rv;poba;] X-Sqldataorigin:[S] X-Streamorigin:[X]] {} 9635415 [] false false map[]  }*/
+	// Download the file.
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Check the status code.
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to download file: %s", resp.Status)
+	}
+
+	//create the outfile
+	out, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	//Write the contents of the response to the output file.
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
